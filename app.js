94bef26b97119a01e3d0874f15d35c3feab2d70b
hochitom <hochitom@icloud.com>
2013-08-01 20:13:35 +0200

    added scroll code

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 (function (app) {
 	app = angular.module('ParallaxScrolling', []);
 
-	app.module('ParallaxCtrl', function PatallaxCtrl ($scope) {
+	app.controller('ParallaxCtrl', function ParallaxCtrl ($scope) {
+		$scope.animation = {};
 
+		window.addEventListener('scroll', function (e) { 
+			var miles = window.scrollY;
+
+			$scope.animation.speed1 = {'-webkit-transform': 'translate3d(' + miles / 20 +'px,' + miles * 1.1 +'px,0)'};
+			$scope.animation.speed2 = {'-webkit-transform': 'translate3d(' + miles / 30 +'px,' + miles * 1.05 +'px,0)'};
+
+			$scope.$digest('animation');
+		})
 	});
 }(window));
